666e9700c984472cc499d14f0d6780fbfe80b669
Adam Zemmoura <adamzemmoura@gmail.com>
2019-06-14 22:12:25 +0200

    updated comments and added pagination links

--- js/script.js
+++ js/script.js
@@ -17,26 +17,21 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 const studentListItemElements = document.querySelectorAll('.student-item')
-console.log(studentListItemElements)
-
 const itemsToShowPerPage = 10
 
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+ * Displays only desired page by hiding all the unwanted items in the list.
+ * 
+ * This method takes a list and page number as parameters and based on the desired
+ * number of items per page, which can be set in itemsToShowPerPage, calculates which 
+ * items to show and sets their display property to 'block'. All others display properties
+ * are set to 'none', hiding them from view. This method should be called each time a new 
+ * page is to be shown ie. when a user clicks one of the pagination buttons.
+ * 
+ * @param   list  {[string]}  The complete list of items.
+ * @param   page  {number}    The page number to show.
 ***/
-
 function showPage(list, page) {
    const startIndex = (page * itemsToShowPerPage) - itemsToShowPerPage
    const endIndex = page * itemsToShowPerPage
@@ -52,11 +47,63 @@ function showPage(list, page) {
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+*  Creates pagination links and appends them to bottom of page
+*
+*  This method creates the required HTML to display the pagination links and appends them to
+*  the bottom of the page. Each link comprises an anchor tag with a clickHandler, which applies
+*  the 'active' CSS class to the newly active page. showPage() is then called passing the newly 
+*  selected page number as an argument. The first page is set to active by default.
+*
+*  @param   list  {[string]}  The full list to be paginated.
 ***/
+function appendPageLinks(list) {
+   
+   let div = document.createElement("div")
+   div.classList.add("pagination")
+   
+   let ul = document.createElement('ul')
+     
+   const numberOfListItemsRequired = Math.floor(list.length / itemsToShowPerPage)
 
+   // create anchor element for each page
+   for(let i = 0; i < numberOfListItemsRequired; i++) {
 
+      let anchorTags = []
 
+      let newAnchorElement = document.createElement("a")
+      newAnchorElement.href = "#"
+      newAnchorElement.innerHTML = `${i + 1}`
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+      // add 'active' class to the first link initially
+      if (i === 0) {
+         newAnchorElement.classList.add("active")
+         
+      } 
+
+      anchorTags.push(newAnchorElement)
+
+      // add click event listener to update selected page
+      anchorTags.forEach( tag => {
+         
+         tag.addEventListener('click', (e) => {
+            document.querySelectorAll('div.pagination a.active').forEach( element => element.classList.remove('active'))
+            const clickedAnchor = e.target 
+            clickedAnchor.classList.add("active")
+            showPage(studentListItemElements, clickedAnchor.innerHTML)
+         })
+         const li = document.createElement("li")
+         li.appendChild(tag)
+         ul.appendChild(li)
+      })
+   }
+
+   div.appendChild(ul)
+   document.querySelector('.page').appendChild(div)
+   
+}
+
+// When DOM content loaded, show page 1 by default and append page links
+document.addEventListener('DOMContentLoaded', () => {   
+   showPage(studentListItemElements, 1)
+   appendPageLinks(studentListItemElements)
+})
